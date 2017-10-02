65c54e63c4d1d041c49645c1300ee53d1551e446
Alex Dougal <adougal@rmn.com>
2017-10-02 09:26:42 -0500

    Commit #3 - Ability to record animations and not just snapshots

--- KonvaAJD-1.js
+++ KonvaAJD-1.js
@@ -10,10 +10,18 @@ var players = [];
 var loopPlayers = [];
 var fieldCopyNumber = 0;
 var timer = 60;
+var globalStartTime;
+var globalCurrentTime;
 var recordingData = [];
 var recordingId;
+var playbackId;
 
-function addBox(x, y, w, h, color, draggable, canClone, addToCounter){
+var loopPlayerBoxes = [];
+
+var recordingTimeInterval = 100;
+var playbackTimeInterval = 80;
+
+function addBox(x, y, w, h, color, draggable, canClone, addToCounter, loopBox){
     var box = new Konva.Rect({
         x: x,
         y: y,
@@ -57,6 +65,9 @@ function addBox(x, y, w, h, color, draggable, canClone, addToCounter){
     if(addToCounter){
         players.push(box);
     }
+    if(loopBox){
+        loopPlayerBoxes.push(box);
+    }
     layer.add(box);
     stage.add(layer);
 }
@@ -83,8 +94,7 @@ function addRecordingBox(x,y,w,h,color){
                 var ijX = players[ij].getAttr("x");
                 var ijY = players[ij].getAttr("y");
                 var ijColor = players[ij].getAttr("fill");
-                var currentTime = timer;
-                var timestampIj = new timestamp(ijX,ijY,ijColor,currentTime);
+                var timestampIj = new Timestamp(ijX,ijY,ijColor,timer);
                 recordingData.push(timestampIj);
             }
             realTimeMillisecondsCurrent = new Date().getTime();
@@ -93,7 +103,7 @@ function addRecordingBox(x,y,w,h,color){
                 clearInterval(recordingId);
             }
             console.log(recordingData);
-        }, 100);
+        }, recordingTimeInterval);
     });
     box.on("mouseover", function() {
         document.body.style.cursor = "pointer"
@@ -105,7 +115,7 @@ function addRecordingBox(x,y,w,h,color){
     stage.add(layer);
 }
 
-function timestamp(x,y,color,minTime){
+function Timestamp(x,y,color,minTime){
     this.x = x;
     this.y = y;
     this.color = color;
@@ -128,17 +138,16 @@ function addBoxToCopyField(x, y, w, h, color){
         fieldCopyNumber+=1;
         var x = 50 + (fieldCopyNumber%3)*250;
         var y = 20 + Math.floor(fieldCopyNumber/3)*600;
-        addBox(x,y,200,550,"white",false,false);
-        addBox(x,y,200,550,"white",false,false);
-        addBox(x,y+100,200,350,"white",false,false);
-        for(i=0;i<players.length;i++){
+        addBox(x,y,200,550,"white",false,false,false,false);
+        addBox(x,y,200,550,"white",false,false,false,false);
+        addBox(x,y+100,200,350,"white",false,false,false,false);
+        for(var i=0;i<players.length;i++){
             var playerX = players[i].getAttr("x") + (fieldCopyNumber%3)*250;
             var playerY = players[i].getAttr("y") + Math.floor(fieldCopyNumber/3)*600;
             var playerW = players[i].getAttr("width");
             var playerH = players[i].getAttr("height");
             var playerColor = players[i].getAttr("fill");
-            console.log(playerX,playerY,playerW,playerH,playerColor,false,false,false);
-            addBox(playerX,playerY,playerW,playerH,playerColor,false,false,false);
+            addBox(playerX,playerY,playerW,playerH,playerColor,false,false,false,false);
         }
     });
     box.on("mouseover", function() {
@@ -151,31 +160,77 @@ function addBoxToCopyField(x, y, w, h, color){
     stage.add(layer);
 }
 
-function destroyAllPlayers(){
-    for (var k = 0; k<players.length;k++){
-        players[k].destroy();
+function destroyAllLoopPlayers(){
+    for (var k = 0; k<loopPlayerBoxes.length;k++){
+        loopPlayerBoxes[k].destroy();
     }
 }
 
 function updateLoopPlayersModel(){
-
-}
-
-function createAllPlayers(){
-    for (var l = 0; k<loopPlayers.length;k++){
-        var x = players[l].getAttr("x");
-        var y = players[l].getAttr("y");
-        var w = players[l].getAttr("width");
-        var h = players[l].getAttr("height");
-        var color = players[l].getAttr("fill");
-        addBox(x,y,w,h,color,false,false,false);
+    loopPlayers = [];
+    globalCurrentTime = new Date().getTime();
+    timer = globalCurrentTime - globalStartTime;
+    for (var increment = 0; increment<recordingData.length; increment++){
+        if(Math.abs(timer - recordingData[increment].minTime) <= playbackTimeInterval/2){
+            loopPlayers.push(recordingData[increment]);
+        }
+    }
+    destroyAllLoopPlayers();
+    createAllLoopPlayers();
+    if (timer > 10000){
+        clearInterval(playbackId);
+        console.log("Killed playback");
     }
 }
-addBox(20,20,15,15,"red",false,true,false);
-addBox(20,50,15,15,"blue",false,true,false);
-addBox(50,20,200,550,"white",false,false,false);
-addBox(50,120,200,350,"white",false,false,false);
+
+function addPlaybackBox(x,y,w,h,color){
+    var box = new Konva.Rect({
+        x: x,
+        y: y,
+        fill: color,
+        stroke: "black",
+        strokeWidth: 4,
+        draggable: false,
+        width: w,
+        height: h
+    });
+
+    box.on("click", function() {
+        globalStartTime = new Date().getTime();
+        globalCurrentTime = globalStartTime;
+        timer = globalCurrentTime - globalStartTime;
+        addBox(300,20,200,550,"white",false,false,false,false);
+        addBox(300,120,200,350,"white",false,false,false,false);
+        playbackId = setInterval(updateLoopPlayersModel, playbackTimeInterval-5);
+    });
+    box.on("mouseover", function() {
+        document.body.style.cursor = "pointer"
+    });
+    box.on("mouseout", function() {
+        document.body.style.cursor = "default"
+    });
+    layer.add(box);
+    stage.add(layer);
+}
+
+function createAllLoopPlayers(){
+    loopPlayerBoxes = [];
+    for (var l = 0; l<loopPlayers.length;l++){
+        console.log(loopPlayers[l]);
+        var x = loopPlayers[l].x + 250;
+        var y = loopPlayers[l].y;
+        var w = 15;
+        var h = 15;
+        var color = loopPlayers[l].color;
+        addBox(x,y,w,h,color,false,false,false,true);
+    }
+}
+addBox(20,20,15,15,"red",false,true,false,false);
+addBox(20,50,15,15,"blue",false,true,false,false);
+addBox(50,20,200,550,"white",false,false,false,false);
+addBox(50,120,200,350,"white",false,false,false,false);
 addBoxToCopyField(20,80,15,15,"grey");
 addRecordingBox(20,110,15,15,"pink");
+addPlaybackBox(20,140,15,15,"green");
 // add the layer to the stage
 stage.add(layer);
